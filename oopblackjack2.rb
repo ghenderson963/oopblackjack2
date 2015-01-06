85b5781545ee7c5d745dc09633873f87e44d56ff
sharkcop <ghenderson963@gmail.com>
2015-01-05 21:26:06 -0800

    Fixed dealer can win on 22 and game ends when someone gets 21

--- oopblackjack2.rb
+++ oopblackjack2.rb
@@ -164,7 +164,6 @@ class Wallet
   def initialize
     @total_cash = 2500
     @current_bet = 0
-
   end
 
   def to_s
@@ -185,30 +184,29 @@ class Game
     @number_of_games = 1
   end
 
-  def switch_players
+  def play
+    system "clear"
+    welcome_screen
+    get_player_name if number_of_games == 1
+    @player = hash_of_players[0]
+    place_bets
+    dealer.build_decks
+    dealer.total_deck.scramble!
+    initial_deal
+    hash_of_players.each do
+      players_turn
+      switch_players
+    end
+    dealer_turn
+    find_winners
+  end
+
+   def switch_players
     if count < hash_of_players.length
       @count = count + 1
       @player = hash_of_players[count]
     end
   end
-
-  def play
-    system "clear"
-  welcome_screen
-  binding.pry
-  get_player_name if number_of_games == 1
-  @player = hash_of_players[0]
-  place_bets
-  dealer.build_decks
-  dealer.total_deck.scramble!
-  initial_deal
-  hash_of_players.each do
-    players_turn
-    switch_players
-  end
-  dealer_turn
-  find_winners
-end
 
   def welcome_screen
     puts "Welcome to BlackJack!"
@@ -227,14 +225,14 @@ end
     end
   end
 
-   def show_cards
-     puts "#{@player} has:"
-     player.wallet.to_s
-     player.list_hand
-     puts " "
-     puts "The dealer has:"
-     dealer.show_flop
-     puts " "
+  def show_cards
+    puts "#{@player} has:"
+    player.wallet.to_s
+    player.list_hand
+    puts " "
+    puts "The dealer has:"
+    dealer.show_flop
+    puts " "
   end
 
     def place_bets
@@ -246,10 +244,10 @@ end
           puts "How much would you like to bet #{player}?"
           puts " "
           bet_amount = gets.chomp.to_i
-       end while bet_amount > player.total_cash
-      player.make_bet(bet_amount)
-     end
-  end
+        end while bet_amount > player.total_cash
+        player.make_bet(bet_amount)
+      end
+    end
 
   def initial_deal
     2.times do
@@ -263,34 +261,34 @@ end
     puts "#{player}'s turn."
     show_cards
     hit_or_stay
-      system "clear"
-      show_cards
-      puts "Hit Enter or Return to proceed"
-      choice = gets.chomp
-    end
+    system "clear"
+    show_cards
+    puts "Hit Enter or Return to proceed"
+    choice = gets.chomp
   end
 
+
   def blackjack?(player_or_dealer)
-      if player_or_dealer.total_card_value == 21
+    if player_or_dealer.total_card_value == 21
       if player_or_dealer.is_a?(Dealer)
-        puts "Sorry, dealer hit Blackjack! #{player_or_dealer.name} loses!"
+        puts "Sorry, dealer hit Blackjack!"
+        hash_of_players.each { |_,player| puts "#{player.name} loses!" }
       else
         puts "Congratulations, you hit blackjack! #{player_or_dealer.name} wins!"
       end
-      # play_again?
     elsif player_or_dealer.is_busted?
       if player_or_dealer.is_a?(Dealer)
-    puts "Dealer busted.  #{player_or_dealer.name} wins!"
-    else
-      puts "Sorry #{player_or_dealer.name} busted.  #{player_or_dealer.name} loses."
+        puts "Dealer busted. "
+        hash_of_players.each { |_,player| puts "#{player.name} wins!" }
+      else
+        puts "Sorry #{player_or_dealer.name} busted. "
+        puts "#{player.name} loses"
+      end
     end
-    # play_again?
-  end
   end
 
 
   def hit_or_stay
-
     blackjack?(player)
     while !player.is_busted?
       puts "Would you like a HIT or would you like to STAY #{player}?"
@@ -311,9 +309,9 @@ end
       puts "Dealing card to #{player.name}: #{new_card}"
       show_cards
       blackjack?(player)
-     end
-     puts "#{player.name} stays at #{player.total_card_value}."
     end
+      puts "#{player.name} stays at #{player.total_card_value}."
+  end
 
 
   def play_again?
@@ -328,6 +326,7 @@ end
       puts " "
       clean_up_card
       @number_of_games = @number_of_games + 1
+      @count = 0
       play
      else
        exit
@@ -338,203 +337,53 @@ end
     puts "Dealers turn."
     blackjack?(dealer)
     while dealer.total_card_value < 17
-      new_card = dealer.add_card(dealer.deal)
+      new_card = dealer.deal
+      dealer.add_card(new_card)
       puts "Dealing card to dealer: #{new_card}"
-      puts "Dealer total is now: #{dealer.total_card_value}"
+      puts "Dealer total is now: ====> #{dealer.total_card_value}"
+      puts " "
       blackjack?(dealer)
-      end
+      sleep(1)
+    end
+      show_dealer_cards
       puts "Dealer stays at #{dealer.total_card_value}"
-   end
+  end
 
-    def clean_up_card
+  def clean_up_card
     hash_of_players.each { |_,player| player.remove_cards }
     dealer.remove_cards
   end
 
-
-  def find_winners
-    hash_of_players.each do |_,player|
-      if player.total_card_value > dealer.total_card_value
-        player.settle_bet("win")
-        puts "#{player} you win!"
-      elsif player.total_card_value < dealer.total_card_value
-        player.settle_bet(FALSE)
-        puts "#{player} you lose!"
-      elsif player.total_card_value == dealer.total_card_value
-        puts "#{player} you and the dealer tied!  No winner!"
-      end
-    end
-      play_again?
+  def show_dealer_cards
+    puts "The dealer has: "
+    dealer.list_hand
   end
-
-  def welcome_screen
-    puts "Welcome to Blackjack!"
-  end
-
-
-
-  # def players_turn
-  #   system "clear"
-  #   puts "#{@hash_of_players[0]} is first."
-  #   self.player = hash_of_players[0]
-  #   hash_of_players.each do |_,player|
-  #     system "clear"
-  #     show_cards
-  #     hit_or_stay
-  #     system "clear"
-  #     show_cards
-  #     puts "Hit Enter or Return to proceed"
-  #     choice = gets.chomp
-  #     switch_players
-  #   end
-  # end
-
-  # def play
-  #   system "clear"
-  #   get_player_name
-
-  #   begin
-  #     system "clear"
-  #     dealer.build_decks
-  #     dealer.total_deck.scramble!
-  #     puts " "
-  #     puts "Let's play Blackjack!"
-  #     puts " "
-  #     place_bets
-  #     clean_up_card
-  #     initial_deal
-  #     players_turn
-  #     system "clear"
-  #     dealers_turn
-  #     find_winners
-  #     puts "Would you like to player again? (Y)es or (N)o"
-  #     play_again = gets.chomp
-  #   end while play_again == 'y'
-  # end
-
-  # def place_bets
-  #   hash_of_players.each do  |_,player|
-  #     bet_amount = 0
-  #     begin
-  #       system "clear"
-  #       puts "You have #{player.total_cash}"
-  #       puts "How much would you like to bet #{player}?"
-  #       puts " "
-  #       bet_amount = gets.chomp.to_i
-  #    end while bet_amount > player.total_cash
-  #    player.make_bet(bet_amount)
-  #   end
-  # end
-
-  # def clean_up_card
-  #   hash_of_players.each { |_,player| player.remove_cards }
-  #   dealer.remove_cards
-  # end
-
-  # def initial_deal
-  #   2.times do
-  #     hash_of_players.each { |_,player| player.add_card(dealer.deal) }
-  #     dealer.add_card(dealer.deal)
-  #   end
-  # end
-
-  # def hit_or_stay
-  #   while @player.total_card_value < 21
-  #     puts "Would you like a HIT or would you like to STAY #{player}?"
-  #     puts "Use the keyboard to type (H) for HIT or (S) to stay"
-  #     answer = gets.chomp.downcase
-  #     if !["h", "s"].include?(answer)
-  #       puts "You must enter s or h"
-  #       next
-  #     end
-  #     if answer == "s"
-  #       puts "You choose to stay!"
-  #       puts " "
-  #       break
-  #     end
-  #     player.add_card(dealer.deal)
-  #     if player.total_card_value == 21
-  #       puts "Blackjack! #{player} wins!"
-  #       sleep(3)
-  #       show_cards
-  #       player.settle_bet("win")
-  #       puts " "
-  #       break
-  #     elsif player.total_card_value > 21
-  #       puts "Busted! #{player} looses!"
-  #       show_cards
-  #       puts " "
-  #       player.settle_bet(FALSE)
-  #       break
-  #     end
-  #     system "clear"
-  #     show_cards
-  #   end
-  # end
-
-
-
-  # def get_player_name
-  #   begin
-  #     puts "how many players are going to play?  Chose 1-4"
-  #     number_of_players = gets.chomp.to_i
-  #   end while number_of_players == 0 || number_of_players > 4
-  #   number_of_players.times do |num|
-  #     puts "What is player#{num + 1}'s name?"
-  #     players_name = gets.chomp.to_s
-  #     hash_of_players[num] = Player.new
-  #     hash_of_players[num].name = "#{players_name}"
-  #   end
-  # end
-
-  # def remove_player
-  #   hash_of_players.delete_if{ |key, value| key == "#{@player}"}
-  # end
-
-  # def dealers_turn
-  #   system "clear"
-  #   puts "Dealers turn!"
-  #   puts "Dealer has:"
-  #   dealer.list_hand
-  #   puts " "
-  #   if dealer.total_card_value == 21
-  #     puts "Dealer has Blackjack!"
-  #     puts " "
-  #   end
-  #   while dealer.total_card_value < 17
-  #     puts "The dealer hits!"
-  #     dealer.add_card(dealer.deal)
-  #     sleep(1)
-  #     puts "The dealer gets:"
-  #     dealer.cards.last.to_s
-  #     puts "For a total of #{dealer.total_card_value}"
-  #     puts " "
-  #     sleep(2)
-  #   end
-  # end
 
   # def find_winners
   #   hash_of_players.each do |_,player|
-  #     if player.total_card_value > 21
-  #       player.settle_bet(FALSE)
-  #       puts "#{player} Busted! #{player} loses"
-  #     elsif dealer.total_card_value > 21
-  #       player.settle_bet("win")
-  #       puts "#{player} you win!  The dealer busted!"
-  #     elsif player.total_card_value > dealer.total_card_value
+  #     if player.total_card_value > dealer.total_card_value
   #       player.settle_bet("win")
   #       puts "#{player} you win!"
   #     elsif player.total_card_value < dealer.total_card_value
   #       player.settle_bet(FALSE)
   #       puts "#{player} you lose!"
   #     elsif player.total_card_value == dealer.total_card_value
-  #       player.settle_bet("win")
   #       puts "#{player} you and the dealer tied!  No winner!"
   #     end
   #   end
+  #     play_again?
   # end
 
+  def find_winners
 
+
+  end
+
+  def welcome_screen
+    puts "Welcome to Blackjack!"
+  end
+
+end
 
 new_game = Game.new
 new_game.play
